0f9186fa3db96c6e325669c1c06a7f9e1d023970
Dennis James Stelmach <dj@dstelmach.com>
2024-04-28 04:57:09 -0400

    More scaffolding for install command.

--- cmd/os/install.go
+++ cmd/os/install.go
@@ -2,6 +2,7 @@ package os
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -14,9 +15,14 @@ import (
 		Hostname
 		Drive args (Show prompt of current disks and context for selection)
 
-	(Parameterize toggle) Run disko to format disks (default) / Attempt mount for install
+	(Parameterize toggle) Run disko to format disks (default)
 		`sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko $DISKO PATH [--arg device '"/dev/nvme0nX"' --arg device2 '"/dev/sdX"']`
 		!Update config to match!
+
+	Generate hardware-configuration.nix
+		`nixos-generate-config --no-filesystems --root /mnt --dir $FlakeLocation/hosts/$Hostname/`
+
+	Attempt mount for install if disko not ran
 
 	Scaffold folders
 		Scaffold /persist folders with correct ownership and permissions
@@ -40,7 +46,51 @@ import (
 type InstallReturn struct {
 }
 
+type diskSelection struct {
+	Arg string
+	Val string
+}
+
 func RunInstall() tea.Msg {
+	// PLACEHOLDERS
+	remoteUrl := "https://github.com/dman926/nixos-config"
+	configAbsPath, direrr := os.Getwd()
+	hostname := "neutron"
+	if direrr != nil {
+		return InstallReturn{}
+	}
+
+	err := cloneConfig(remoteUrl, configAbsPath)
+	if err != nil {
+		return InstallReturn{}
+	}
+	disks := discPrompt(configAbsPath, hostname)
+	err = runDisko(configAbsPath, hostname, disks)
+	if err != nil {
+		return InstallReturn{}
+	}
+	err = generateHardwareConfig(configAbsPath, hostname)
+	if err != nil {
+		return InstallReturn{}
+	}
+	err = scaffoldPersist(configAbsPath, hostname)
+	if err != nil {
+		return InstallReturn{}
+	}
+	err = installNixos(configAbsPath, hostname)
+	if err != nil {
+		return InstallReturn{}
+	}
+	err = scaffoldSecrets(configAbsPath, hostname)
+	if err != nil {
+		return InstallReturn{}
+	}
+	err = installNixos(configAbsPath, hostname)
+	if err != nil {
+		return InstallReturn{}
+	}
+	reboot()
+
 	return InstallReturn{}
 }
 
@@ -51,8 +101,34 @@ func cloneConfig(remoteUrl string, configAbsPath string) (err error) {
 	return
 }
 
-func discPrompt(configAbsPath string, hostname string) {
+func discPrompt(configAbsPath string, hostname string) []diskSelection {
 	// TODO
+	return []diskSelection{}
+}
+
+func runDisko(configAbsPath string, hostname string, disks []diskSelection) (err error) {
+	baseArgs := []string{
+		"sudo", "nix", "--experimental-features", "nix-command flakes", "run", "github:nix-community/disko", "--", "--mode", "disko", configAbsPath + "/hosts/" + hostname + "/disks.nix",
+	}
+	combinedArgs := make([]string, len(disks)*3)
+	for i := 0; i < len(disks); i++ {
+		combinedArgs[i*3] = "--arg"
+		combinedArgs[i*3+1] = disks[i].Arg
+		combinedArgs[i*3+2] = "\"" + disks[i].Val + "\""
+	}
+	combinedArgs = append(baseArgs[1:], combinedArgs...)
+
+	diskoCmd := exec.Command(baseArgs[0], combinedArgs...)
+	err = diskoCmd.Run()
+
+	return
+}
+
+func generateHardwareConfig(configAbsPath string, hostname string) (err error) {
+	generateCmd := exec.Command("nixos-generate-config", "--no-filesystems", "--root", "/mnt", "--dir", fmt.Sprintf("%s/hosts/%s", configAbsPath, hostname))
+	err = generateCmd.Run()
+
+	return
 }
 
 func scaffoldPersist(configAbsPath string, hostname string) (err error) {
